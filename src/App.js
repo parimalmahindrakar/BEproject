61313adb6103434ba6c92164ceb4cb817b5704d7
parimalmahindrakar <parimalm4653@gmail.com>
2022-03-03 16:28:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Categories from './components/Categories';
+import LoadingCircle from './components/LoadingCircle';
+
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+function returnTrue() {
+  sleep(7000)
+  return true;
+}
+
+function App() {
+  const[post, setPost] = useState(null)
+
+  useEffect(() =>{
+    fetch("https://jsonplaceholder.typicode.com/posts/1").then(res => res.json()
+      .then(res =>{
+        sleep(7000)
+        const {body} = res
+        setPost(body)
+      })
+    )
+  })
+
+
+  return (
+    <div className="App"> 
+      {post ? <Categories/>: <LoadingCircle/>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Categories.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import lips from '../images/lips.jpg'
+export default function Categories() {
+  return (
+    
+    <div className="categories__bars">
+
+        
+        <div className="category__bar">
+            <div className="text">
+                <span>Lips</span>
+            </div>
+            <div className="image">
+                <img src={lips}/>
+            </div>
+        </div>
+
+        <div className="category__bar">
+            <div className="text">
+                <span>Lips</span>
+            </div>
+            <div className="image">
+                <img src={lips}/>
+            </div>
+        </div>
+
+        <div className="category__bar">
+            <div className="text">
+                <span>Lips</span>
+            </div>
+            <div className="image">
+                <img src={lips}/>
+            </div>
+        </div>
+        <div className="category__bar">
+            <div className="text">
+                <span>Lips</span>
+            </div>
+            <div className="image">
+                <img src={lips}/>
+            </div>
+        </div>
+
+        <div className="category__bar">
+            <div className="text">
+                <span>Lips</span>
+            </div>
+            <div className="image">
+                <img src={lips}/>
+            </div>
+        </div>
+
+        <div className="category__bar">
+            <div className="text">
+                <span>Lips</span>
+            </div>
+            <div className="image">
+                <img src={lips}/>
+            </div>
+        </div>
+
+
+       
+       
+    </div>
+
+
+
+  )
+}
